73416f4f46e2fb6522c1a0eb6dcc944dfbbc9e75
Felipe Sferra <86127586+FelipeSferra@users.noreply.github.com>
2023-06-26 21:00:19 -0300

    Delete index.php (test of functions)

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dataAtual = new DateTime();  // Cria um objeto DateTime com a data atual
-
-$dataAtual->add(new DateInterval('P5D'));  // Adiciona um intervalo de 5 dias à data atual
-
-echo $dataAtual->format('Y-m-d');
